260bab476ee620cdd8072120527891bf5805c691
Casper Örnberg <115638514+CasperOrnberg01@users.noreply.github.com>
2025-04-23 21:17:48 +0300

    Update app.js

--- aweb-project/backend/app.js
+++ aweb-project/backend/app.js
@@ -4,10 +4,13 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const usersRoutes = require('./routes/users'); //users reitit (phase3)
 
 // Käytetään middlewarejä: JSON-pyyntöjen parsing ja CORS
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/users', usersRoutes); // rekisteröi users reitit (phase3)
 
 // Testireitti testaamaan palvelimen vastausta
 app.get('/api/hello', (req, res) => {
