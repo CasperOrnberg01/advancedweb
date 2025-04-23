d9a8e914f2bdd53357eee38f0833f16b5672909e
Casper Örnberg <115638514+CasperOrnberg01@users.noreply.github.com>
2025-04-23 21:28:09 +0300

    Update Nav.jsx

--- aweb-project/frontend/src/components/Nav.jsx
+++ aweb-project/frontend/src/components/Nav.jsx
@@ -3,15 +3,30 @@ import React from 'react';
 // Navigaatio palkki, kaksi nappia: items ja orders
 //currentScene = mikä näkymä
 // onNavigate funktio toiseen näkymään siirtymiseen
-function Nav({ currentScene, onNavigate }) {
+function Nav({ currentScene, onNavigate, userRole }) {
   return (
     <nav>
-      <button onClick={() => onNavigate('items')} disabled={currentScene === 'items'}>
+      <button
+        onClick={() => onNavigate('items')}
+        disabled={currentScene === 'items'}
+      >
         Items
       </button>
-      <button onClick={() => onNavigate('orders')} disabled={currentScene === 'orders'}>
+      <button
+        onClick={() => onNavigate('orders')}
+        disabled={currentScene === 'orders'}
+      >
         Orders
       </button>
+      {/* register scene jos rooli manager Phase 3 lisäys */}
+      {userRole === 'manager' && (
+        <button
+          onClick={() => onNavigate('register')}
+          disabled={currentScene === 'register'}
+        >
+          Register
+        </button>
+      )}
     </nav>
   );
 }
